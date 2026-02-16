7abea716f32617912f0e26ac2dc8bd71a686730c
Luiz Felipe Ditommaso <luuizditommaso@gmail.com>
2026-02-16 10:35:16 +0000

    js-perguntasfrequentes

--- js/script.js
+++ js/script.js
@@ -26,3 +26,24 @@ function ativarProduto(parametro) {
 }
 
 paramentros.forEach(ativarProduto);
+
+//Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+  console.log(resposta);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
